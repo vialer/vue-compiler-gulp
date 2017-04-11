b87969f07de820c46005aab7ae3a4b03502ee8dd
Jeroen van Veen <j.veenvan@gmail.com>
2017-04-11 14:50:38 +0200

    Adding initial code.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # gulp-vue
-Fast and uncomplicated .vue file compiler for Gulp.
+Plain and simple fast(blazingly if you wish) and uncomplicated .vue file compiler
+for Gulp. Compiles all templates to an object. Template namespace is configurable
+and filled dynamically based on your template app structure. No inline stylesheets
+and javascripts. This is bad practice.
+
+# Install
+With [npm](https://www.npmjs.com/) do:
+
+    npm install gulp-vue --save-dev
+
+
+Then use it in your gulp file like:
+
+    const vue = require('gulp-vue')
+
+    gulp.task('templates', 'Builds all Vue components.', () => {
+        gulp.src('./src/js/**/*.vue')
+        .pipe(vue('templates.js', {
+            namespace: 'global.tpl',
+            prefixStart: 'modules',
+            prefixIgnore: ['templates'],
+        }))
+        .on('error', notify.onError('Error: <%= error.message %>'))
+        .pipe(size(extend({title: 'templates'}, sizeOptions)))
+        .pipe(gulp.dest(BUILD_DIR))
+        .pipe(ifElse(watcher, livereload))
+    })
---
+++ index.js
@@ -0,0 +1,84 @@
+'use strict'
+
+const compiler = require('vue-template-compiler')
+const Buffer = require('buffer').Buffer
+const Concat = require('concat-with-sourcemaps')
+const extend = require('lodash.assign')
+const os = require('os')
+const path = require('path')
+const through = require('through2')
+
+var transpile = require('vue-template-es2015-compiler')
+
+let defaults = {
+    sep: os.EOL,
+    namespace: 'window.templates',
+    extension: '.vue',
+    prefixStart: '',
+    prefixIgnore: [],
+    vue: {
+        preserveWhitespace: false,
+    },
+}
+
+function toFunction(code) {
+    return transpile('function render() {' + code + '}')
+}
+
+
+function gulpVue(name, config) {
+    let options = extend({}, defaults, config || {})
+    let concat, fileName, firstFile
+    let templateNamespace = `${options.namespace} = {}`
+
+    function combineFiles(file, encoding, next) {
+        if (!firstFile) {
+            firstFile = file
+            // Default path to first file basename.
+            fileName = name || path.basename(file.path)
+            concat = new Concat(!!file.sourceMap, fileName, options.sep)
+            concat.add(file.relative, templateNamespace, file.sourceMap)
+        }
+
+        let baseName = path.basename(file.path, options.extension)
+        let templateData = file.contents.toString('utf8')
+
+        try {
+            let _path = file.path.split('/')
+            let pathPrefixIndex = _path.indexOf(options.prefixStart)
+            if (pathPrefixIndex === -1) throw 'Cannot find prefix start!'
+            let templatePrefix = _path.slice(pathPrefixIndex + 1, _path.length - 1)
+            templatePrefix = templatePrefix.filter((i) => {
+                return !options.prefixIgnore.includes(i)
+            })
+            let templateName = `${templatePrefix.join('_')}_${baseName}`
+            let compiled = compiler.compile(templateData, options.vue)
+
+            compiled.errors.forEach(function(msg) {
+                this.emit('error', new Error(msg))
+            })
+
+            let jsTemplate = `${options.namespace}.${templateName}={render: ${toFunction(compiled.render)}, staticRenderFns: [${compiled.staticRenderFns.map(toFunction).join(',')}]}`
+            concat.add(file.relative, jsTemplate, file.sourceMap)
+        } catch (_error) {
+            this.emit('error', new Error(_error))
+        }
+
+        next()
+    }
+
+    function flush(next) {
+        if (firstFile) {
+            let joinedFile = firstFile.clone()
+            joinedFile.path = path.join(options.cwd || firstFile.base, fileName)
+            joinedFile.base = options.base || firstFile.base
+            joinedFile.contents = new Buffer(concat.content)
+            this.push(joinedFile)
+        }
+        next()
+    }
+
+    return through.obj(combineFiles, flush)
+}
+
+module.exports = gulpVue
